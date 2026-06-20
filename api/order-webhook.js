8e2aaae4af18ab3f8b2316d29e269e81fcaf94e7
اكتب_هنا_اليوزر_نيم_الخاص_بك <yasenyasenya123586.744@gmail.com>
2026-06-20 11:00:40 +0300

    Update project features

--- api/order-webhook.js
+++ api/order-webhook.js
@@ -1,4 +1,4 @@
-// api/order-webhook.js
+// api/order-webhook.js - triggered rebuild for new env variables
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
